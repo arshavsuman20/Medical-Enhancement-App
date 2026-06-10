1551f0c449ba95f8c341be0a0b69947f23979ed3
Arshav Suman <arshavsuman@gmail.com>
2026-06-10 08:28:58 +0530

    Updated Changes

--- convert_model.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-
-# Load old model
-model = tf.keras.models.load_model("models/unet.h5", compile=False)
-
-# Export as SavedModel (NEW WAY)
-model.export("models/final_model")
-
-print("Model converted successfully!")
